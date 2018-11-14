1834667398174daa98aaf9329bf3bcd9cb07d3db
LeDankWolffe <33060767+LeDankWolffe@users.noreply.github.com>
2018-11-14 11:30:57 +0000

    Commented
    just commented on some stuff in the code

--- 00_Template/sketch.js
+++ 00_Template/sketch.js
@@ -2,23 +2,24 @@ function setup() {
   createCanvas(500,500);
     background(0,0,0);
     frameRate(30);
+    // ^^^ Basic setup
 
 }
 
 function draw() {
-  //fill(216,213,197);
-  customShape();
+  customShape(); //calling the shape drawing function
+
+  //this is the refresh function, so every 10 seconds it will automatically refresh
   setTimeout(function() {
   location.reload();
 }, 10000);
-  //for (var i = frame(300); i < frameRate; ++=); {
-//console.log(x);
    }
 
   function customShape() {
     let size = random(200);
     let x = random(500);
     let y = random(500);
+    // This determines the size and position of each individual circle
 
     fill(random(255),random(255),random(255),random(255));
     ellipse(x,y,size,size);
@@ -30,6 +31,8 @@ function draw() {
     ellipse(x,y,size*0.25,size*0.25);
     fill(random(255),random(255),random(255),random(255));
     ellipse(x,y,size*0.2,size*0.2);
+
+    //every circle that is drawn within one another
 
 
   }
